557c082b463cfa00816a91d6ded04bf5dabefb95
No2p971 <nathanvivien.o@gmail.com>
2026-02-18 15:57:38 +0100

    vidéo 3

---
+++ TP3-PDO-PHP/header.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.101.0">
+    <title>Jumbotron Template · Bootstrap v4.6</title>
+
+
+    <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css"> <!-- bon lien -->
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-us=er-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Personnalisation de template -->
+    <link href="jumbotron.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <a class="navbar-brand" href="#">Ma Bibliothèque</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">Gestion des genres</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Liste des genres</a>
+          <a class="dropdown-item" href="#">Ajouter un genre</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">Gestion des auteurs</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Liste des auteurs</a>
+          <a class="dropdown-item" href="#">Ajouter un auteur</a>
+          <a class="dropdown-item" href="#">Rechercher un auteur</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">Gestion des nationalités</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="listNationalies.php">Liste des nationalités</a>
+          <a class="dropdown-item" href="#">Ajouter une nationalité</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
--- TP3-PDO-PHP/index-1.php
+++ TP3-PDO-PHP/index-1.php
@@ -1,74 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.101.0">
-    <title>Jumbotron Template · Bootstrap v4.6</title>
+<?php include "header.php";
 
-
-    <!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css"> <!-- bon lien -->
-
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-us=er-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Personnalisation de template -->
-    <link href="jumbotron.css" rel="stylesheet">
-  </head>
-  <body>
-    
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand" href="#">Ma Bibliothèque</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">Gestion des genres</a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Liste des genres</a>
-          <a class="dropdown-item" href="#">Ajouter un genre</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">Gestion des auteurs</a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Liste des auteurs</a>
-          <a class="dropdown-item" href="#">Ajouter un auteur</a>
-          <a class="dropdown-item" href="#">Rechercher un auteur</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">Gestion des nationalités</a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Liste des nationalités</a>
-          <a class="dropdown-item" href="#">Ajouter une nationalité</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+?>
 
 <main role="main">
 
@@ -137,12 +69,6 @@
     </div>
 </main>
 
-<footer class="container">
-  <p>&copy; Company 2017-2024</p>
-</footer>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<?php include "footer.php";
 
-</body>
-</html>
+?>
